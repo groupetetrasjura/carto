e006614e81f0ac6a518e74a42be096cb70f2d8b6
Gaëtan Duhamel <gaetan_duhamel@natural-solutions.eu>
2024-12-02 12:49:03 +0100

    feat: add api routes to fetch maptiler credentials  from env vars

---
+++ src/app/api/credentials/route.tsx
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const credentials = {
+      maptilerApiKey: process.env.NEXT_PUBLIC_MAPTILER_API_KEY,
+      maptilerMapId: process.env.NEXT_PUBLIC_MAPTILER_MAP_ID,
+    };
+
+    console.log("credentials", credentials);
+
+    return NextResponse.json(credentials);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch credentials", details: error },
+      { status: 500 }
+    );
+  }
+}
