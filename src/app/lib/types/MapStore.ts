10593496b3180504208fb0922b3435fa61c34069
Jordane_NS <jordane_minaya@natural-solutions.eu>
2025-02-28 15:14:21 +0100

    feat: map layers can be activate or not... ... implement store logic

--- src/app/lib/types/MapStore.ts
+++ src/app/lib/types/MapStore.ts
@@ -1,8 +1,21 @@
 import { ViewState } from "react-map-gl";
 
+export type LayersVisibility = {
+  "zonages-zqfs-source": boolean;
+  "protected-areas-source": {
+    ENS: boolean;
+    RNR: boolean;
+    RNN: boolean;
+  };
+  "swiss-protected-areas-source": boolean;
+  "other-appb-source": boolean;
+};
+
 export interface MapStoreState {
   viewState: ViewState;
+  layersVisibility: LayersVisibility;
   actions: {
     setViewState(viewState: ViewState): void;
+    toggleLayer(layerId: string, subLayerId?: string): void;
   };
 }
